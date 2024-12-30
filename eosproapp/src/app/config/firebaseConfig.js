7cb7835f09a09871455884bb833790abd51e5c29
chris-vaz <chrisvas2001@gmail.com>
2024-12-30 23:32:15 +0300

    Set up basic middleware and Firebase Config

--- eosproapp/src/app/config/firebaseConfig.js
+++ eosproapp/src/app/config/firebaseConfig.js
@@ -1,5 +1,8 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
+// Firebase configuration using environment variables
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -7,7 +10,11 @@ const firebaseConfig = {
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app;
+const analytics = getAnalytics(app);
+
+export { app, analytics };
