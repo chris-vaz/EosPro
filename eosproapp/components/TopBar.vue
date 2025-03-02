a80d172f5b4eed5c3a3ce6e4df801c8f3754665d
chris-vaz <chrisvas2001@gmail.com>
2025-03-02 14:54:14 +0300

    Added app optional topbar

---
+++ eosproapp/components/TopBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="w-full bg-blue-600 text-white p-4 flex justify-between items-center">
+        <h1 class="text-xl font-bold">EOSPRO</h1>
+        <nav>
+            <ul class="flex space-x-4">
+                <li>
+                    <NuxtLink to="/" class="hover:underline">Home</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/jobs" class="hover:underline">Jobs</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/profile" class="hover:underline">Profile</NuxtLink>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
