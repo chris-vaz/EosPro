df4b179420ffca6542275a407171361605087173
chris-vaz <chrisvas2001@gmail.com>
2025-03-05 15:03:41 +0300

    Added Nuxt Icons and other required dependancies

--- eosproapp/nuxt.config.ts
+++ eosproapp/nuxt.config.ts
@@ -3,6 +3,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  modules: [
+    'nuxt-icon',
+    // other modules...
+  ],
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [
