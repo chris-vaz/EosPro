91df5cd1d5edfbdbf3c56afbf5d885aa37677a0e
chris-vaz <chrisvas2001@gmail.com>
2025-04-06 16:18:21 +0300

    Added tailwind config

---
+++ eosproapp/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx,vue}', // Make sure this includes the location of your Vue files
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
